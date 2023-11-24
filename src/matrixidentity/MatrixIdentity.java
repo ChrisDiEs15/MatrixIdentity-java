f8161856d38c63fc90f72f8da38487decf27a04b
Chris <Chris@192.168.0.192>
2023-11-23 18:30:42 -0600

    Finish matriz identity

--- src/matrixidentity/MatrixIdentity.java
+++ src/matrixidentity/MatrixIdentity.java
@@ -4,6 +4,8 @@
  */
 package matrixidentity;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Chris
@@ -15,6 +17,79 @@ public class MatrixIdentity {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner tamano = new Scanner(System.in);
+
+        System.out.println("Programa que identifica si una matriz es identidad");
+
+        System.out.println("Ingrese la fila de la matriz 1");
+        int fila = tamano.nextInt();
+        System.out.println("Ingrese el tamano de las columnas ");
+        int columna = tamano.nextInt();
+
+        /*Func to determine if the matriz is square */
+        if (isSquare(fila, columna)) {
+            int[][] matrix = matrixCreator(fila, columna);
+            showMatrix(matrix, fila, columna);
+            if(isIdentity(matrix, matrix.length)){
+                System.out.println("La matriz es identidad");
+            }else{
+            System.out.println("La matriz no es identidad");
+            }
+        } else {
+            System.out.println("Por favor ingrese una nueva matriz");
+        }
+
+    }
+
+    /*func to create a matriz n*m*/
+    public static int[][] matrixCreator(int fila, int columna) {
+        Scanner datosMatrix = new Scanner(System.in);
+
+        int[][] matrixCreator = new int[fila][columna];
+        for (int k = 0; k < fila; k++) {
+            for (int l = 0; l < columna; l++) {
+                System.out.println("Ingrese el dato de la fila " + k + " de la columna " + l);
+                int nDato = datosMatrix.nextInt();
+                matrixCreator[k][l] = nDato;
+            }
+        }
+
+        return matrixCreator;
+    }
+
+    public static boolean isIdentity(int mat[][], int N) {
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (row == col && mat[row][col] != 1) {
+                    return false;
+                } else if (row != col && mat[row][col] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSquare(int fila, int columna) {
+
+        if (fila == columna) {
+            System.out.println("Se determinara a continuacion si la matriz es una matriz identidad");
+        } else {
+            System.out.println("No se puede determinar si la matriz no es cuadrada");
+            return false;
+        }
+
+        return true;
     }
     
+    public static void showMatrix(int matrix[][], int fila, int columna) {
+
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
